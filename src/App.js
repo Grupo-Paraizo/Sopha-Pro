9e563917c184c6701177b1f2db8818d3d6024994
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-07-18 20:41:42 -0300

    excluindo arquivos desnecessários devido a mudança na tela

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
 import React from 'react';
 import './App.css';
-import ButtonList from './components/ButtonList';
-import ButtonMap from './components/ButtonMap';
 import SearchInput from './components/SearchInput';
-import SearchFilter from './components/SearchFilter';
-import ServicesOptions from './components/ServicesContainer';
 import GeolocationServicesContainer from './components/GeolocalionServicesContainer';
 
 function App() {
   return (
     <div className="mainContainer">
       <section className="sideContainer">
-        <div className="containerBtn">
-          <ButtonList />
-          <ButtonMap />
-        </div>
         <SearchInput />
-        <section className="sideMenuContainer">
-          <SearchFilter />
-          <ServicesOptions />
-        </section>
       </section>
       <section>
         <GeolocationServicesContainer />
