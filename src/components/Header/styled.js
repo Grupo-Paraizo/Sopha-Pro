89025b216de3a8b78b4e3a0fab9e7120394eb5c0
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-07-28 22:50:29 -0300

    modificando component header

--- src/components/Header/styled.js
+++ src/components/Header/styled.js
@@ -10,4 +10,26 @@ export const Header = styled.header`
       align-items:flex-start;
    }
 `;
-export const NavMenu = styled.ul``;
+export const Menu = styled.ul`
+   height:6rem;
+   display:flex;
+   align-items:center;
+      li{
+         list-style:none;
+         display: inline-block;
+         margin-right:5rem;
+      }
+         
+         a{
+            font-size:2rem;
+            font-weight:bold;
+            color:#a5a3ab;
+            text-decoration:none;
+            transition:.2s;
+         }
+            a:hover{
+               color:#1b1b1f;
+            }
+
+
+`;
