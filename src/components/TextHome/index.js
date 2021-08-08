9f18cacd0bb83cae380abd0a3ce9da695e8b8c46
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-08-07 23:40:09 -0300

    adicionando logo e textos da home page

--- src/components/TextHome/index.js
+++ src/components/TextHome/index.js
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Container } from './styled';
+import { Container, LogoContainer, TextContainer } from './styled';
 
 const TextHome = () => {
    return (
       <Container>
-         <div id="logo">
+         <LogoContainer>
             <img src="../../../SophaLogo1.svg" alt=""></img>
             <img src="../../../SophaLogo2.svg" alt=""></img>
-         </div>
-         <div id="description">
+         </LogoContainer>
+
+         <TextContainer>
             <h3>COMPRAR MÓVEIS PLANEJADOS NUNCA FOI TÃO SIMPLES</h3>
             <p>
                Conheça o Sopha Planejados, com ele seus clientes acompanham
                todo o processo de implantação dos seus novos móveis. Como e
                onde quiser, na palma da mão.
             </p>
-         </div>
+         </TextContainer>
       </Container>
    )
 }
--- src/components/TextHome/styled.js
+++ src/components/TextHome/styled.js
@@ -3,14 +3,25 @@ import styled from "styled-components";
 export const Container = styled.div`
    height:24rem;
    width:57rem;
-      div#logo{
-         margin-bottom:1.5rem;
-      }
-      div#description{
-         padding:1rem;
-         font-size:2.5rem;
-         }
-            h3{
-               font-size:2rem;
-            }
+    h3{
+     font-size:24px;
+   }
+   p{
+    font-size:1.4rem;
+   }
 `;
+
+export const LogoContainer = styled.div`
+   margin-bottom:1.5rem;
+`;
+
+export const TextContainer = styled.div`
+   h3{
+      font-size:2.8rem;
+      font-weight:400;
+   }
+   p{
+      font-size:1.8rem;
+   }
+`;
+
