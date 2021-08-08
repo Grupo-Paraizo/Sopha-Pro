8b6b11c8701562f46c89b030d7397768051c0668
Bruno Moreira <bruno_moreira.19@outlook.com>
2021-08-08 00:40:25 -0300

    finalizando layout tela home

--- src/Pages/HomePage/styled.js
+++ src/Pages/HomePage/styled.js
@@ -1,9 +1,38 @@
 import styled from "styled-components";
 
 export const Page = styled.main`
-   width:100%;
-   height:100%;
-   display:flex;
+   width:100vw;
+   height:70vh;
    background: url('../../../bgSopha.jpg')center;
    background-size: cover;
+      div.header{
+         display:flex;
+         justify-content: flex-end;
+         height:15vh;
+         padding-right:10rem;
+      }
+      section.main{
+         display:flex;
+         width:70vw;
+         margin:5rem auto 0 auto;
+         align-items:stretch;
+      }
+      div.contentLeft{
+      }
+
+      div.phone{
+         margin-top: -10rem;
+      }
+      footer{
+         display:flex;
+         align-items:center;
+         justify-content:center;
+         margin-top:-4rem;
+      }
+         a.footerLink{
+            text-decoration:none;
+            font-size:3rem;
+            color:#0f0f0f; 
+            padding-bottom: 5rem;
+         }
 `;
--- src/components/TextHome/styled.js
+++ src/components/TextHome/styled.js
@@ -3,25 +3,21 @@ import styled from "styled-components";
 export const Container = styled.div`
    height:24rem;
    width:57rem;
-    h3{
-     font-size:24px;
-   }
-   p{
-    font-size:1.4rem;
-   }
 `;
 
 export const LogoContainer = styled.div`
-   margin-bottom:1.5rem;
+   margin-bottom:2rem;
 `;
 
 export const TextContainer = styled.div`
    h3{
       font-size:2.8rem;
       font-weight:400;
+      color:#fafafa
    }
    p{
       font-size:1.8rem;
+      color:#fafafa
    }
 `;
 
